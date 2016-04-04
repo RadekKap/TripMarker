768ca95ef883c8bdf24c7f42c9382336415ed144
Adrian <adrian_kaczmarek94@wp.pl>
2016-04-04 19:40:14 +0200

    Added Sign Up activity

--- app/src/main/java/jawas/tripmarker/LoginActivity.java
+++ app/src/main/java/jawas/tripmarker/LoginActivity.java
@@ -1,17 +1,51 @@
 package jawas.tripmarker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Firebase.setAndroidContext(this);
+
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
+    public void signUp(View view){
+        Intent goToSignUp = new Intent(this, SignUpActivity.class);
+        startActivity(goToSignUp);
+    }
+
+    public void signIn(){
+        EditText email = (EditText)findViewById(R.id.email);
+        EditText password = (EditText)findViewById(R.id.password);
+
+        Firebase database = new Firebase("https://tripmarker.firebaseio.com/");
+
+        database.authWithPassword(email.getText().toString(),
+                password.getText().toString(), new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+
+            }
+
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+
+            }
+        });
+    }
 }
---
+++ app/src/main/java/jawas/tripmarker/SignUpActivity.java
@@ -0,0 +1,43 @@
+package jawas.tripmarker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.Map;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void signUp(View view) {
+        EditText email = (EditText) findViewById(R.id.email);
+        EditText password = (EditText) findViewById(R.id.password);
+
+        Firebase firebase = new Firebase("https://tripmarker.firebaseio.com/");
+
+        firebase.createUser(email.getText().toString(), password.getText().toString(),
+            new Firebase.ValueResultHandler<Map<String, Object>>() {
+                @Override
+                public void onSuccess(Map<String, Object> stringObjectMap) {
+
+                }
+
+                @Override
+                public void onError(FirebaseError firebaseError) {
+
+                }
+            });
+    }
+}
