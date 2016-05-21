5478e5b9258f07b33b99d218d64fba7bc681a7ad
Radosław Kapłon <radziuk96@gmail.com>
2016-05-21 15:14:36 +0200

    dodanie funkcji usuwania do listy oraz zmiana guzików

--- app/src/main/java/jawas/tripmarker/profile/VisitedPlacesFragment.java
+++ app/src/main/java/jawas/tripmarker/profile/VisitedPlacesFragment.java
@@ -1,6 +1,8 @@
 package jawas.tripmarker.profile;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,7 +13,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -30,11 +34,11 @@ public class VisitedPlacesFragment extends Fragment {
 
     private static String UID;
     private ListView list ;
-    private ArrayAdapter<String> adapter ;
+    private MyListAdapter adapter ;
     public ArrayList<String> places;
+    public int pom=0;
+    FloatingActionButton btnAdd;
 
-    ImageButton btnAdd;
-    Firebase database;
 
     public static VisitedPlacesFragment newInstance() {
         VisitedPlacesFragment fragment = new VisitedPlacesFragment();
@@ -68,12 +72,17 @@ public class VisitedPlacesFragment extends Fragment {
                     if(snapshot.getValue(t)!=null)
                     { places = snapshot.getValue(t);
 
-                    adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, places);
-                    list.setAdapter(adapter);}
+                  /*  adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, places);
+                    list.setAdapter(adapter);*/
+
+                        list.setAdapter(new MyListAdapter(getActivity(), R.layout.list_item, places));
+
+                        int i=list.getChildCount();
+                        Log.i("ERROR", "DataSnapshot error " +i);
+                    }
 
                 }
             }
-
             @Override
             public void onCancelled(FirebaseError firebaseError) {
                 Log.i("ERROR", "DataSnapshot error");
@@ -90,7 +99,7 @@ public class VisitedPlacesFragment extends Fragment {
         View view =inflater.inflate(R.layout.fragment_visited_places, container, false);
 
         list = (ListView)view.findViewById(R.id.listView);
-        btnAdd = (ImageButton) view.findViewById(R.id.imageButton);
+        btnAdd = (FloatingActionButton) view.findViewById(R.id.imageButton);
         final EditText placetextField = (EditText) view.findViewById(R.id.placeTextField);
 
         getlist();
@@ -106,8 +115,14 @@ public class VisitedPlacesFragment extends Fragment {
 
 
                     places.add(placetextField.getText().toString());
-                    adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, places);
+                 /*   adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, places);
                     list.setAdapter(adapter);
+
+                    */
+                  //  list.deferNotifyDataSetChanged();
+                  //  list.forceLayout();
+                    list.setAdapter(new MyListAdapter(getActivity(),R.layout.list_item,places) );
+
 
                     FirebaseRef.getDbContext().child("users").child(UID).child("VP").setValue(places);
                     Toast.makeText(getActivity().getApplicationContext(), "You have added " + placetextField.getText().toString() + " to your places", Toast.LENGTH_LONG).show();
@@ -125,5 +140,71 @@ public class VisitedPlacesFragment extends Fragment {
         return view;
     }
 
+    private class MyListAdapter extends ArrayAdapter<String>{
+        private int layout;
+        private final Context context;
+        private final List<String> values;
+        public MyListAdapter(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            layout=resource;
+            this.context=context;
+            values=objects;
+        }
 
+        @Override
+        public View getView(final int position,View convertView, ViewGroup parent){
+          Viewholder mainviewhold=null;
+           if(convertView==null){
+               LayoutInflater inflater=LayoutInflater.from(getContext());
+               convertView=inflater.inflate(layout,parent,false);
+               Viewholder viewhold=new Viewholder();
+               viewhold.imagebutt=(FloatingActionButton)convertView.findViewById(R.id.imageButtonlistitem);
+               viewhold.title=(TextView)convertView.findViewById(R.id.textViewtolist);
+               viewhold.title.setText(values.get(position));
+               viewhold.imagebutt.setOnClickListener(new View.OnClickListener() {
+
+                   @Override
+                   public void onClick(View v) {
+                      // Toast.makeText(getActivity().getApplicationContext(), "You have to type place viewhold " + position, Toast.LENGTH_LONG).show();
+                      String temp=values.get(position);
+                       places.remove(values.get(position));
+
+                       list.setAdapter(new MyListAdapter(getActivity(), R.layout.list_item,places) );
+
+                       FirebaseRef.getDbContext().child("users").child(UID).child("VP").setValue(places);
+                       Toast.makeText(getActivity().getApplicationContext(), "You have removed " + temp + " from your places", Toast.LENGTH_LONG).show();
+                   }
+               });
+                convertView.setTag(viewhold);
+           }else
+           {
+               mainviewhold=(Viewholder)convertView.getTag();
+               mainviewhold.title.setText(values.get(position));
+               mainviewhold.imagebutt.setOnClickListener(new View.OnClickListener() {
+
+                   @Override
+                   public void onClick(View v) {
+                     //  Toast.makeText(getActivity().getApplicationContext(), "You have to type place mainviewhold " + position, Toast.LENGTH_LONG).show();
+                       String temp=values.get(position);
+                       places.remove(values.get(position));
+
+                       list.setAdapter(new MyListAdapter(getActivity(),R.layout.list_item,places) );
+
+                       FirebaseRef.getDbContext().child("users").child(UID).child("VP").setValue(places);
+                       Toast.makeText(getActivity().getApplicationContext(), "You have removed " + temp + " from your places", Toast.LENGTH_LONG).show();
+                   }
+               });
+
+           }
+
+
+
+            return convertView;
+        }
+    }
+
+    public class Viewholder{
+        FloatingActionButton imagebutt;
+        TextView title;
+    }
 }
